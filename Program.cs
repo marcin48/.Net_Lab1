70a5e50570b39ffa148b46a5d5d7fe2e44954383
Marcin Mironiski <272611@student.pwr.edu.pl>
2025-03-06 17:43:32 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -51,39 +51,3 @@ namespace MyConsoleApp
         }        
     }
 }
-
-
-/*
-Console.WriteLine("Hello, World!");
-
-
-int limit = 30;
-FizzBuzz fizzBuzz = new FizzBuzz(limit);
-fizzBuzz.Run();
-
-
-class FizzBuzz
-{
-    private int _limit;
-
-    public FizzBuzz(int limit)
-    {
-        _limit = limit;
-    }
-
-    public void Run()
-    {
-        for (int i = 1; i <= _limit; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0)
-                Console.WriteLine("FizzBuzz");
-            else if (i % 3 == 0)
-                Console.WriteLine("Fizz");
-            else if (i % 5 == 0)
-                Console.WriteLine("Buzz");
-            else
-                Console.WriteLine(i);
-        }
-    }
-}
-*/
